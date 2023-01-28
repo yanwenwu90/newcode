69cff63cf87d423572fe14af7a40c365621462b6
yanwenwu90 <1239181126@qq.com>
2023-01-28 15:23:26 +0800

    modify test case

--- test1.js
+++ test1.js
@@ -4,12 +4,12 @@ const _isCard = (number) => {
   return regIdCard.test(number);
 };
 
+// 测试用例
 console.log(_isCard("21062319980907888X"));
 console.log(_isCard("21062319980907888x"));
 console.log(_isCard("210623199809078882"));
 console.log(_isCard("210623199809078"));
 console.log(_isCard("21062319980907888"));
 console.log(_isCard("21062319980907888r"));
-console.log(_isCard("130922199011146038"));
 console.log(_isCard("13092219901114603V"));
 console.log(_isCard("Y1062319980907888X"));
